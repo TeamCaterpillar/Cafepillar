d3966a226f17e11d2e47c2ffc115962e72314858
ethereal-chxn <ethchan@ucdavis.edu>
2024-11-23 13:01:57 -0800

    made changes to card slot's draw() to draw a rectangle instead of a circle
    - changed color of card slot

--- Cafepillar_Game/scripts/card_drop_point.gd
+++ Cafepillar_Game/scripts/card_drop_point.gd
@@ -1,5 +1,6 @@
 extends Marker2D
 
+var slot_dimensions:Vector2 = Vector2(25.0, 35.0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,12 +12,15 @@ func _process(delta: float) -> void:
 	pass
 
 func _draw() -> void:
-	draw_circle(Vector2.ZERO, 25.0, Color.WHITE)
+	#draw_circle(Vector2.ZERO, 25.0, Color.WHITE)
+	draw_rect(Rect2(-slot_dimensions / 2, slot_dimensions),Color.WHITE)
 
 func select() -> void:
 	for child in get_tree().get_nodes_in_group("CardZone"):
 		child.deselect()
-	modulate = Color.RED
+	#modulate = Color.RED
+	modulate = Color8(211, 131, 114, 170)
 
 func deselect() -> void:
-	modulate = Color.WHITE
+	#modulate = Color.WHITE
+	modulate =  Color8(211, 131, 114, 255)
