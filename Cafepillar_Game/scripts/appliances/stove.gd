50b17b7aba6e470f06b4d63ec10a40e5b5d7cf96
Minh Nguyen <minh@campus-034-209.ucdavis.edu>
2024-12-05 16:47:50 -0800

    reimplemented Stove as a scene

--- Cafepillar_Game/scripts/appliances/stove.gd
+++ Cafepillar_Game/scripts/appliances/stove.gd
@@ -1,10 +1,45 @@
-extends Control
+extends TextureRect
+class_name Slot
 
-@export var slot1 : TextureRect
-@export var slot2 : TextureRect
-# add however many slots are needed, can dynmaically size the slots based on recipe size
+@export var deck : GCardHandLayout
 
-var slot1_resource : Resource
-var slot2_resource : Resource
+# @onready var yes_button: TextureButton = $"../../Tray/YesButton"
 
-# Add cooking logic below
+var card_resource
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	connect("child_entered_tree", _on_child_entered_tree)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+	# uncomment this when we have a food card
+	# only let yes button be visible if a food card is in slot
+	#if name == "TraySlot":
+		# check if food card is in slot
+	#	if get_child_count() > 0:
+	#		var food_card = get_child(0)
+			# only allow player to send a food card and not smt else
+	#		if food_card.is_in_group("Food"):
+	#			yes_button.visible = true
+	#		else:
+	#			yes_button.visible = false
+	#	else:
+	#		yes_button.visible = false
+
+
+func _on_child_entered_tree(node: Node) -> void:
+	if node.is_in_group("Ingredient"):
+		card_resource = node.card_resource
+		print("Dropped " , card_resource.name, " into " , get_parent().name , " slot.")
+	else:
+		print("ERROR WHY IS ", node, " BEING ADDED!!")
+		_move_card_back_to_deck(node)
+
+
+func _move_card_back_to_deck(child : Node) -> void:
+	remove_child(child)
+	deck.add_child(child)
+	 
