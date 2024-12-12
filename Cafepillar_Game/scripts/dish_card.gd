ae3dd1d2c699ac88fdc38699ee876ce3a772a85f
tdha7 <timothy.ha.2016@gmail.com>
2024-12-11 23:37:30 -0800

    inventory

--- Cafepillar_Game/scripts/dish_card.gd
+++ Cafepillar_Game/scripts/dish_card.gd
@@ -1,12 +1,14 @@
-extends Panel
+extends Button
 
 @export var food_name: String
 @export var food_condition: String
 @onready var food_sprite: Sprite2D = $FoodIcon
 @onready var food_label: Label = $FoodNameLabel
 @onready var food_color: ColorRect = $FoodColor
+@onready var completed_dish_inventory: CompletedDishInventory = $"../../"
 
 func _ready() -> void:
+	self.connect("pressed", Callable(self, "_on_Dish_pressed"))
 	food_label.text = ""
 	food_sprite.visible = false
 	var food_icon_path = "res://assets/cards/" + str(food_name) + ".png"
@@ -45,3 +47,19 @@ func format_string(input: String) -> String:
 		# Capitalize the first letter of each word and make the rest lowercase
 		words[i] = words[i].capitalize()
 	return " ".join(words)  # Join the words with spaces
+
+
+func _on_Dish_pressed() -> void:
+	completed_dish_inventory.close_inventory()
+	# delivering_food.emit()
+	completed_dish_inventory.selected_food_to_deliver = self
+	print("clicked on ", food_name, food_condition)
+	
+	# Logic:
+	# click card to deliver
+	# idk: maybe send another signal that allows player to click on customer?
+	# click customer
+	# sends a signal only if customer.current_order == dish_card.food_name
+	# drop currency based on dish_card.food_condition
+	# remove item from the inventory array in Game Manager and queue_free() it from grid container
+	# reset selected food in completed_dish_inventory to null
