fd488f0bb23784f80c94ce2e871cf6f5eb772898
Aroshia <mchlu@ucdavis.edu>
2024-12-13 15:28:49 -0800

    Revert "minor end of day fix"
    This reverts commit a3985b89da6824646dac98ae217e5da9b4867704.

--- Cafepillar_Game/scripts/characters/character.gd
+++ Cafepillar_Game/scripts/characters/character.gd
@@ -8,7 +8,7 @@ const DEFAULT_JUMP_VELOCITY = -400
 var movement_speed = DEFAULT_MOVE_VELOCITY
 var jump_velocity = DEFAULT_JUMP_VELOCITY
 
-#@onready var char_sprite:Sprite2D = $Sprite2D
+@onready var char_sprite:Sprite2D = $Sprite2D
 @onready var animation_player : AnimationPlayer = get_child(0).get_child(0)
 
 var target_position : Vector2 = Vector2.ZERO
