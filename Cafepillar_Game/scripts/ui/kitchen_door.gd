70042e5c3aa47a055689e6fcdf720116feafefa8
ethereal-chxn <ethchan@ucdavis.edu>
2024-12-13 21:59:46 -0800

    more kitchen door work

--- Cafepillar_Game/scripts/ui/kitchen_door.gd
+++ Cafepillar_Game/scripts/ui/kitchen_door.gd
@@ -5,3 +5,11 @@ extends Area2D
 
 func _ready() -> void:
 	indicator.visible = false
+
+
+func _on_mouse_entered() -> void:
+	indicator.visible = true
+
+
+func _on_mouse_exited() -> void:
+	indicator.visible = false
